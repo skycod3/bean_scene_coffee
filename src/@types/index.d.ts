48ad1c1e21ec887f5936b53a14a3324e428b9a0b
skycod3 <jeamcrv@hotmail.com>
2023-08-03 09:07:38 -0300

    Add CustomProperties type

---
+++ src/@types/index.d.ts
@@ -0,0 +1,7 @@
+export interface CustomProperties extends React.CSSProperties {
+  "--fluid-block-min"?: string;
+  "--fluid-block-base"?: string;
+  "--fluid-block-max"?: string;
+  "--growth-factor"?: string;
+  "--flow-space"?: string;
+}
